19f9c2e82063361ebba087aaaf7ee10252026060
yamori <yamori@riso.co.jp>
2022-07-30 23:12:22 +0900

    all done

---
+++ api/serializers.py
@@ -0,0 +1,23 @@
+# データベースとブラウザからの入出力関係をカスタマイズする
+
+from dataclasses import field
+from pyexpat import model
+from rest_framework import serializers
+from .models import Task
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': { 'write_only': True, 'required': True}}
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class TaskSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%H', read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
+    class Meta:
+        model = Task
+        fields = ('id','title','created_at','updated_at')
